f1c5ed8ee437b5e831bfbe22ad1252cd09dc47b7
itcast <@itcast.cn>
2022-05-22 16:23:28 +0800

    2.0

--- src/main/java/com/ka/controller/HelloController.java
+++ src/main/java/com/ka/controller/HelloController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("")
     public String hello(){
-        System.out.println("hello,world");
-        return "OK";
+        System.out.println("hello,world222222222");
+        return "OK3";
     }
 }
