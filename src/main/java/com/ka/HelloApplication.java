13f2a36e21114d80c82b557eaf3d2b5d52808670
itcast <@itcast.cn>
2022-05-22 15:14:14 +0800

    1.0

---
+++ src/main/java/com/ka/HelloApplication.java
@@ -0,0 +1,17 @@
+package com.ka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: HelloApplication
+ * @Description: TODO
+ * @author: 名字
+ * @date: 2022/5/20  21:33
+ */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class,args);
+    }
+}
---
+++ src/main/java/com/ka/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.ka.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: HelloController
+ * @Description: TODO
+ * @author: 名字
+ * @date: 2022/5/20  21:32
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping("")
+    public String hello(){
+        System.out.println("hello,world");
+        return "OK";
+    }
+}
